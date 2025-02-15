7e3768b78090d388165de2657a2be07c1acb2057
LyviaFernandes <lyviafernandesborges@gmail.com>
2025-02-15 12:06:48 -0300

    Create posinegorzero.py
    positivo
    negativo
    ou zero?

---
+++ posinegorzero.py
@@ -0,0 +1,14 @@
+print('Olá Humano.')
+
+num = int(input('Digite um número e direi se é negativo, positivo ou zero'))
+
+if num >= 1:
+    print('Você possui um número positivo')
+elif num < 0:
+    print('Você possui um número negativo')
+elif num == 0:
+    print('Este número é zero')
+else:
+    print('inválido')
+
+print('Fim:))')
